232d74ade00480f335992524004f47dd764ee6cf
mabn1 <mababativan@udistrital.edu.co>
2026-06-18 13:49:45 -0500

    Initial Spring Boot project

---
+++ src/main/java/co/edu/udistrital/agendapersonal/AgendaPersonalApplication.java
@@ -0,0 +1,13 @@
+package co.edu.udistrital.agendapersonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaPersonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaPersonalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/udistrital/agendapersonal/AgendaPersonalApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.udistrital.agendapersonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaPersonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
